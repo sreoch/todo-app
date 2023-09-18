b67317f26e86262f5aec2f149ca2a9f3fbc95b1c
Scott Reoch <scottreoch10@hotmail.co.uk>
2023-09-18 18:37:35 +0100

    added drag/drop functionality. little bit buggy so might rework later

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function addTask() {
 	} else {
 		let li = document.createElement('li');
 		li.innerHTML = inputBox.value;
+		li.className = 'drag-item';
 		listContainer.appendChild(li);
 		let span = document.createElement('span');
 		span.className = 'material-symbols-outlined';
@@ -14,12 +15,85 @@ function addTask() {
 		li.appendChild(span);
 	}
 	inputBox.value = '';
+	saveData();
 }
 
 listContainer.addEventListener('click', function (e) {
 	if (e.target.tagName === 'LI') {
 		e.target.classList.toggle('checked');
+		saveData();
 	} else if (e.target.tagName === 'SPAN') {
-		e.target.parentElement.remove();
+		// e.target.parentElement.remove();
+		saveData();
 	}
 });
+
+//TODO - REWORK THIS AREA AS ITS A BIT BUGGY
+let draggedItem = null;
+
+//add event listeners for drag and drop events
+listContainer.addEventListener('dragstart', handleDragStart);
+listContainer.addEventListener('dragover', handleDragOver);
+listContainer.addEventListener('drop', handleDrop);
+
+// Drag start event handler
+function handleDragStart(event) {
+	draggedItem = event.target;
+	event.dataTransfer.effectAllowed = 'move';
+	event.dataTransfer.setData('text/html', draggedItem.innerHTML);
+	event.target.style.opacity = '0.5';
+}
+
+// Drag over event handler
+function handleDragOver(event) {
+	event.preventDefault();
+	event.dataTransfer.dropEffect = 'move';
+	const targetItem = event.target;
+	if (
+		targetItem !== draggedItem &&
+		targetItem.classList.contains('drag-item')
+	) {
+		// const boundingRect = targetItem.getBoundingClientRect();
+		// const offset = boundingRect.y + boundingRect.height / 2;
+		// if (event.clientY - offset > 0) {
+		// 	targetItem.style.borderBottom = 'solid 2px #000';
+		// 	targetItem.style.borderTop = '';
+		// } else {
+		// 	targetItem.style.borderTop = 'solid 2px #000';
+		// 	targetItem.style.borderBottom = '';
+		// }
+	}
+}
+
+// Drop event handler
+function handleDrop(event) {
+	event.preventDefault();
+	const targetItem = event.target;
+	if (
+		targetItem !== draggedItem &&
+		targetItem.classList.contains('drag-item')
+	) {
+		if (
+			event.clientY >
+			targetItem.getBoundingClientRect().top + targetItem.offsetHeight / 2
+		) {
+			targetItem.parentNode.insertBefore(draggedItem, targetItem.nextSibling);
+		} else {
+			targetItem.parentNode.insertBefore(draggedItem, targetItem);
+		}
+	}
+	targetItem.style.borderTop = '';
+	targetItem.style.borderBottom = '';
+	draggedItem.style.opacity = '';
+	draggedItem = null;
+}
+
+function saveData() {
+	localStorage.setItem('data', listContainer.innerHTML);
+}
+
+function showTask() {
+	listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
