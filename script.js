8e32d087224b14155e628a50879561d1d70d42c8
Scott Reoch <scottreoch10@hotmail.co.uk>
2023-09-19 17:34:11 +0100

    added my summary box which works fully

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
+const summaryContainer = document.getElementById('summary-container');
 
 // Add a task
 function addTask() {
@@ -17,6 +18,29 @@ function addTask() {
 	}
 	inputBox.value = '';
 	saveData();
+	getTaskCount();
+}
+
+// Get number of children elements in list
+
+function getTaskCount() {
+	const tasks = listContainer.querySelectorAll(`:not(.checked)`); //query selector to get the elements that ARENT checked
+	const taskCount = tasks.length; //get length of the list without the checked ones
+	console.log(tasks);
+	const div = document.createElement('div');
+	div.className = 'tasks-remaining';
+	div.innerHTML = `Tasks remaining: ${taskCount}`;
+
+	// Check if a 'tasks-remaining' div already exists
+	const existingDiv = summaryContainer.querySelector('.tasks-remaining');
+
+	if (existingDiv) {
+		// If it exists, update its content
+		existingDiv.innerHTML = `Tasks remaining: ${taskCount}`;
+	} else {
+		// If it doesn't exist, append the new div
+		summaryContainer.appendChild(div);
+	}
 }
 
 // Check or remove tasks
@@ -24,11 +48,35 @@ listContainer.addEventListener('click', function (e) {
 	if (e.target.tagName === 'LI') {
 		e.target.classList.toggle('checked');
 		saveData();
+		getTaskCount();
+		getCheckedTasks();
 	} else if (e.target.tagName === 'SPAN') {
 		e.target.parentElement.remove();
 		saveData();
+		getTaskCount();
+		getCheckedTasks();
 	}
 });
+
+// Get amount of checked tasks
+function getCheckedTasks() {
+	const checkedTasks = listContainer.getElementsByClassName('checked');
+	const numberOfCheckedTasks = checkedTasks.length;
+	const div = document.createElement('div');
+	div.className = 'completed-tasks';
+	div.innerHTML = `Completed tasks: ${numberOfCheckedTasks}`;
+
+	// Check if a 'tasks-remaining' div already exists
+	const existingDiv = summaryContainer.querySelector('.completed-tasks');
+
+	if (existingDiv) {
+		// If it exists, update its content
+		existingDiv.innerHTML = `Completed tasks: ${numberOfCheckedTasks}`;
+	} else {
+		// If it doesn't exist, append the new div
+		summaryContainer.appendChild(div);
+	}
+}
 
 // Storing data so it's stored on refresh/restart
 function saveData() {
@@ -40,3 +88,4 @@ function showTask() {
 }
 
 showTask();
+getTaskCount();
